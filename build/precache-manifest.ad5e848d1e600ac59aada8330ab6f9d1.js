6e8858b5601804d2fd096d4bbb31d8570e7f00cb
kamirake <jose@origensmedia.com>
2019-02-01 04:37:40 +0000

    updating the build folder

---
+++ build/precache-manifest.ad5e848d1e600ac59aada8330ab6f9d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4d4dcf487a95d7438c4",
+    "url": "/static/js/main.c4d4dcf4.chunk.js"
+  },
+  {
+    "revision": "075a841c0051da3a5750",
+    "url": "/static/js/1.075a841c.chunk.js"
+  },
+  {
+    "revision": "c4d4dcf487a95d7438c4",
+    "url": "/static/css/main.0c43bb28.chunk.css"
+  },
+  {
+    "revision": "075a841c0051da3a5750",
+    "url": "/static/css/1.965ef054.chunk.css"
+  },
+  {
+    "revision": "ce91c1cda2c6bf29325cae1965d25029",
+    "url": "/index.html"
+  }
+];
